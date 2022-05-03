f3c0260e47ce6e098da962e7cac7b8d96e4d94c6
Egor_Maslov <egor.maslov1996@yandex.ru>
2022-05-03 11:20:50 +0300

    v1.8

--- internal/config/decode_test.go
+++ internal/config/decode_test.go
@@ -13,7 +13,14 @@ func TestTakeConfigFromYaml(t *testing.T) {
 
 	tableCase := []checkRes{
 		{
-			"./testdata/config.yml",
+			"./testdata/testconfig2.yml",
+			&Config{
+				Server: Server{
+					Host: "0.0.0.0",
+				},
+			},
+		},
+		{"./testdata/testconfig1.yml",
 			&Config{
 				Server: Server{
 					Host: "0.0.0.0",
@@ -31,8 +38,7 @@ func TestTakeConfigFromYaml(t *testing.T) {
 					Mode:     "disable",
 				},
 			},
-		},
-		{"./testdata/config.yml",
+		}, {"./testdata/testconfig3.yml",
 			&Config{
 				Server: Server{
 					Host: "0.0.0.0",
@@ -53,17 +59,45 @@ func TestTakeConfigFromYaml(t *testing.T) {
 		},
 	}
 
-	// default 8080
-
 	for _, val := range tableCase {
 		result, err := TakeConfigFromYaml(val.pathToFail)
 		if err != nil {
-			t.Fatal()
+			t.Fatal(err)
+		}
+
+		switch {
+
+		case result.Server.Host == "":
+			result.Server.Host = "0.0.0.0"
+
+		case result.Server.Port.Http == "":
+			result.Server.Port.Http = "8080"
+
+		case result.Db.Driver == "":
+			result.Db.Driver = "postgres"
+
+		case result.Db.User == "":
+			result.Db.User = "postgres"
+
+		case result.Db.Password == "":
+			result.Db.Password = "postgres"
+
+		case result.Db.HostDb == "":
+			result.Db.HostDb = "localhost"
+
+		case result.Db.PortDb == "":
+			result.Db.PortDb = "5432"
+
+		case result.Db.Dbname == "":
+			result.Db.Dbname = "postgres"
+
+		case result.Db.Mode == "":
+			result.Db.Mode = "disable"
+
 		}
 
 		if *result != *val.expectedRes {
-			t.Errorf("Error. Expected %v, got %v", val.expectedRes, result)
-
+			t.Errorf("Error. Expected %v, got %v", result, val.expectedRes)
 		}
 
 	}
